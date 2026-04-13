c93d277adab581ead2c905b68a28e92b1ea1006e
John Ejoke, CC <ejoke.john4socanalyst@outlook.com>
2026-04-13 09:49:04 +0100

    Update outputs.tf

--- SOC_Operations/adaptive-defense-soc-lab/outputs.tf
+++ SOC_Operations/adaptive-defense-soc-lab/outputs.tf
@@ -1,1 +1,30 @@
 
+output "vpc_id" {
+  description = "ID of the SOC VPC"
+  value       = aws_vpc.soc_vpc.id
+}
+
+output "subnet_id" {
+  description = "ID of the public subnet"
+  value       = aws_subnet.soc_public_subnet.id
+}
+
+output "security_group_id" {
+  description = "ID of the SOC security group"
+  value       = aws_security_group.soc_sg.id
+}
+
+output "instance_public_ip" {
+  description = "Public IP address of the EC2 target"
+  value       = aws_instance.soc_analysis_node.public_ip
+}
+
+output "instance_id" {
+  description = "EC2 instance ID"
+  value       = aws_instance.soc_analysis_node.id
+}
+
+output "ssh_command" {
+  description = "SSH command to connect to the instance"
+  value       = "ssh -i soc_key_v2.pem ubuntu@${aws_instance.soc_analysis_node.public_ip}"
+}
